d7ce6c99a9b7fd3ad7a5f6a7b559da122fa7b6fb
Lucas <luccasgsouza@gmail.com>
2025-04-27 11:35:43 -0300

    Adicionando título da aba do navegador

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 
-st.markdown("<h1 style='text-align: center;'> 📈 Dashboard de Apostas Esportivas do LC </h1>", unsafe_allow_html=True)
+st.set_page_config(
+    page_title="Dashboard de Apostas Esportivas do LC",
+    page_icon="🏆",
+    layout="centered"
+)
+
+st.markdown("<h1 style='text-align: center;'>Dashboard de Apostas Esportivas do LC</h1>", unsafe_allow_html=True)
 
 st.write('Bem-vindo ao seu sistema de apostas!')
-
-st.write('Cuida para cuidar!')
-
-
