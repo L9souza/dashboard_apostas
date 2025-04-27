f6693554dbafcc0371ebb64864ff2b1b31a736d5
Lucas <luccasgsouza@gmail.com>
2025-04-27 11:39:43 -0300

    Atualizando texto

--- app.py
+++ app.py
@@ -3,12 +3,10 @@ import streamlit as st
 st.set_page_config(
     page_title="Dashboard de Apostas Esportivas do LC",
     page_icon="🏆",
-    layout="centered"
+    layout="wide"  # <- Aqui, usar "wide" ao invés de "centered"
 )
 
 st.title('Dashboard de Apostas Esportivas do LC')
 
 st.write('Bem-vindo ao seu sistema de apostas!')
 st.write('Cuida para cuidar!')
-
-
