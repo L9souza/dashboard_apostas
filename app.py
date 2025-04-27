0e8910879f04d448f143a12b48bd4951f48e5c9c
Lucas <luccasgsouza@gmail.com>
2025-04-27 11:33:41 -0300

    Atualizando texto

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 
-st.markdown("<h1 style='text-align: center;'>Dashboard de Apostas Esportivas do LC</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'> 📈 Dashboard de Apostas Esportivas do LC </h1>", unsafe_allow_html=True)
 
 st.write('Bem-vindo ao seu sistema de apostas!')
+
+st.write('Cuida para cuidar!')
 
 
