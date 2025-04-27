b25b99f2e4ce79aa4a5dccda1177bd914421ec42
Lucas <luccasgsouza@gmail.com>
2025-04-27 11:30:50 -0300

    Atualizando texto

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.title('🏆 Dashboard de Apostas Esportivas do LC')
 
 st.write('Bem-vindo ao seu sistema de apostas do LC!')
-st.write('Cuida para cuida pae!')
+st.write('Cuida para cuidar pae!')
 
 
