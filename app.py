1ec4cd6c47032c2b5f406f843eb0eedc001418bb
Lucas <luccasgsouza@gmail.com>
2025-04-27 11:36:37 -0300

    Atualizando texto

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="Dashboard de Apostas Esportivas do LC",
-    page_icon="🏆",
-    layout="centered"
-)
-
-st.markdown("<h1 style='text-align: center;'>Dashboard de Apostas Esportivas do LC</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'> 📈 Dashboard de Apostas Esportivas do LC </h1>", unsafe_allow_html=True)
 
 st.write('Bem-vindo ao seu sistema de apostas!')
+
+st.write('Cuida para cuidar!')
+
+
